5ba8eae9f736815451a7f1a480880a6beb10d779
Michael Naish <mnaish@uwo.ca>
2025-01-29 15:19:07 -0500

    Update for Hall effect sensor and optional WiFi to increase power consumption

--- C3-Beacon/C3-Beacon.ino
+++ C3-Beacon/C3-Beacon.ino
@@ -19,7 +19,7 @@
  *
  *  The low bits pulsing at ~38 kHz can be detected by a TSOP IR receiver and drive its output low
  *  If the output of a TSOP IR receiver is connected to the Rx pin on the MSEduino and the MSEduino 
- *  is onfigured to read this serial port at 2400 baud, the MSEduino will read a "U" (0x55), or 
+ *  is configured to read this serial port at 2400 baud, the MSEduino will read a "U" (0x55), or 
  *  whatever ASCII character is defined.
  *
  *  TSOP 32338 IR Receiver pin 1 is output, 2 is Vcc, 3 is Gnd; pin one is on left side when looking 
@@ -27,99 +27,138 @@
  *
  *  While running, the built-in LED (GPIO8) will toggle on and off with each transmission burst.
  * 
- *  If a limit switch connected to GPIO9 is pressed, the beacon will stop tramsitting for ~30 s and 
- *  the built-in LED will stop blinking.
- *
- *  The supply voltage is monitored on GPIO1 (which must be connected to 3V3 pin). If the voltage 
- *  drops below ~3V, the ESP32 will will enter deep sleep to protect the battery. The battery will 
- *  have to be recharged and reattached to reenable the beacon.
- *
+ *  If a source of magnetic flux (i.e., a magnet) is not near the Hall effect sensor, the beacon
+ *  will stop transmitting and the built-in LED will stop blinking. 
+ *  
+ *  If necessary, power consumption can be increased by enabling the onboard WiFi peripheral and
+ *  periodically scanning for networks. This may be required to keep some rechargable batteries
+ *  from turning off due to insufficient load.
+ *   
  *  Required settings for USB serial output to function properly (In Tools menu)
  *   USB CDC on Boot: Enabled
- *   JTAG Adapter: Integrated USB JTAG
  *
  */
 
+#define INCREASE_POWER_USE                            // Enable if necessary to keep battery on
+#define SELF_TEST                                     // Enable to test with connected IR detector
+
 #include <Arduino.h>
-#include <esp_task_wdt.h>
-#include "hal/wdt_types.h"
-#include "hal/wdt_hal.h"
+#ifdef INCREASE_POWER_USE
+#include "WiFi.h"
+#endif
 
 // Constants
-const uint8_t cVCCPin = 1;                            // GPIO used to monitor input voltage
+const uint8_t cHallPin = 4;                           // GPIO for Hall effect sensor
 const uint8_t cCarrierPin = 5;                        // GPIO for 38 kHz carrier
 const uint8_t cTXPin = 6;                             // GPIO for TX1
-const uint8_t cRXPin = 7;                             // GPIO for RX1 (assigned, but unused)
+const uint8_t cRXPin = 21;                            // GPIO for RX1 (for testing with IR receiver only)
 const uint8_t cLEDPin = 8;                            // built-in LED (ON on LOW)
-const uint8_t cLimitSwitchPin = 9;                    // GPIO for limit switch input (active LOW)
-const uint8_t cLimitSwitchGndPin = 10;                // GPIO used as ground for limit switch
 const uint8_t cPWMResolution = 10;                    // bit resolution for carrier PWM (1-14 bits)
 const uint8_t cTXData = 0x55;                         // byte to be sent: U = 0b01010101
+const uint16_t cMinADC = 500;                         // minimum acceptable ADC value (removes ADC artifacts)
+const uint16_t cLowerHallLimit = 2100;                // lowest acceptable raw ADC value from Hall sensor
+const uint16_t cUpperHallLimit = 2700;                // highest acceptable raw ADC value from Hall sensor
+const uint16_t cMaxMissingHallCounts = 5;             // allowable out of range Hall sensor measurements
 const uint16_t cNumBytes = 1;                         // number of bytes to transmit in a burst
-const uint16_t cVCCThreshold = 3700;                  // ~3.0 VDC
-const uint32_t cBurstSpacing = 100;                   // milliseconds between bursts
-const uint32_t cOffTime = 30000;                      // duration of transmission deactivation, in milliseconds
+const uint32_t cBurstInterval = 100;                  // milliseconds between bursts
 const uint32_t cPWMFrequency = 38000;                 // carrier frequency in Hz
 const float cDutyCycle = 0.5;                         // duty cycle of carrier (0-1)
 
+#ifdef INCREASE_POWER_USE
+const uint16_t cScanInterval = 3000;                  // WiFi network scan interval in milliseconds
+#endif
+#ifdef SELF_TEST
+const uint8_t cIRGndPin = 10;                         // GPIO used for IR receiver ground (testing only)
+const uint8_t cIRVCCPin = 20;                         // GPIO used for IR receiver supply (testing only)
+#endif
+
 // Variables
 uint32_t nextBurst;                                   // time of next tranmission burst
-uint32_t restartTime;                                 // time to restart transmission after deactivation
-hw_timer_t * pTimer = NULL;                           // pointer to timer used by timer interrupt
+uint16_t hallVal;                                     // raw ADC value from Hall effect sensor
+uint16_t hallMissingCount = 0;                        // number of 
 bool runState = true;                                 // 0 = stopped; 1 = running
+#ifdef INCREASE_POWER_USE
+uint32_t nextScan;                                    // time of next scan for WiFi networks
+bool scanState = false;                               // 0  = clear network list; 1 = scan for networks
+#endif
+#ifdef SELF_TEST
+uint8_t receivedData = 0;                             // data received from IR receiver (byte)
+#endif
 
 void setup() {
-  pinMode(cVCCPin, INPUT);                            // Configure voltage monitoring pin as input
-  pinMode(cLimitSwitchPin, INPUT_PULLUP);             // Configure limit switch input
-  pinMode(cLimitSwitchGndPin, OUTPUT);                // Configure limit switch ground pin
-  digitalWrite(cLimitSwitchGndPin, LOW);              // Set limit switch ground reference
-  pinMode(cLEDPin, OUTPUT);                           // Configure built-in LED pin as output
-  pinMode(cTXPin, OUTPUT);                            // Configure TX pin as output
+  pinMode(cHallPin, INPUT);                           // configure Hall monitoring pin as input
+  pinMode(cLEDPin, OUTPUT);                           // configure built-in LED pin as output
+  pinMode(cTXPin, OUTPUT);                            // configure TX pin as output
 
-  // disable watchdog timer
-  wdt_hal_context_t rtc_wdt_ctx = RWDT_HAL_CONTEXT_DEFAULT();
-  wdt_hal_write_protect_disable(&rtc_wdt_ctx);
-  wdt_hal_disable(&rtc_wdt_ctx);
-  wdt_hal_write_protect_enable(&rtc_wdt_ctx);
-  esp_task_wdt_deinit();
+#ifdef INCREASE_POWER_USE
+  WiFi.mode(WIFI_STA);                                // initialize WiFi in station mode
+  WiFi.disconnect();                                  // disconnect from all access points
+  delay(100);                                         // ensure WiFi peripheral has started
+#endif
+#ifdef SELF_TEST
+  Serial.begin(9600);                                 // enable standard serial to output received bytes
+  pinMode(cIRGndPin, OUTPUT);                         // configure IR receiver ground pin as output
+  digitalWrite(cIRGndPin, LOW);                       // set low (ground)
+  pinMode(cIRVCCPin, OUTPUT);                         // configure IR receiver supply pin as output
+  digitalWrite(cIRVCCPin, HIGH);                      // set high (VCC)
+#endif
 
-  // configure LEDC to generate carrier signal with specified frequency, resolution and duty cycle
+  // configure LEDC to generate carrier signal with specified frequency, resolution, and duty cycle
   ledcAttach(cCarrierPin, cPWMFrequency, cPWMResolution);
   ledcWrite(cCarrierPin, (1 << cPWMResolution) * cDutyCycle); // 2^cPWMResolution * cDutyCycle
 
-  // configure UART1 with desired parameters: 2400 baud rate, 8 data bits, no parity, 1 stop bit
+  // configure UART2 with desired parameters: 2400 baud rate, 8 data bits, no parity, 1 stop bit
   Serial1.begin(2400, SERIAL_8N1, cRXPin, cTXPin);
   nextBurst = millis();                               // initialize burst timing
 }
 
 void loop() {
   uint32_t curTime = millis();                        // get current time
-  
-  // disable transmission when limit switch is engaged
-  if (!digitalRead(cLimitSwitchPin)) {                // if the limit switch is LOW, switch is engaged
-    runState = false;                                 // deactivate transmission
-    restartTime = curTime + cOffTime;                 // set time for transmission reactivation
-    digitalWrite(cLEDPin, HIGH);                      // turn off built-in LED
-  }
 
-  // transmit character(s) periodically
-  if (curTime - nextBurst < cBurstSpacing) {          // wait number of milliseconds to next burst
+  // update Hall effect sensor reading and transmit character(s) periodically
+  if (curTime - nextBurst < cBurstInterval) {         // wait number of milliseconds to next burst
+    hallVal = analogRead(cHallPin);                   // read Hall effect sensor
+    // if Hall effect sensor measurement is within allowable limits (i.e., magnet is detected)
+    if (((hallVal >= cMinADC) && (hallVal <= cLowerHallLimit)) || (hallVal >= cUpperHallLimit)) {
+      if (!runState) {                                // if transmission is disabled
+        runState = true;                              // enable transmission
+        hallMissingCount = 0;                         // reset missing Hall measurement count
+      }
+    }
+    else {
+      // if too many Hall effect sensor readings are out of range (i.e., magnet is not detected)
+      if (hallMissingCount++ > cMaxMissingHallCounts) {
+        runState = false;                             // disable transmission
+        digitalWrite(cLEDPin, HIGH);                  // turn off built-in LED
+      }
+    }
     if (runState) {                                   // if transmission is active
-      for (int i = 0; i < cNumBytes; i++) {           
+      for (uint16_t i = 0; i < cNumBytes; i++) {           
         Serial1.write(cTXData);                       // load UART1 TX FIFO buffer with data
       }
       digitalWrite(cLEDPin, !digitalRead(cLEDPin));   // toggle built-in LED
     }
-    nextBurst += cBurstSpacing;                       // update time for next burst
-    uint16_t vcc = analogRead(cVCCPin);               // read input voltage
-    if (vcc <= cVCCThreshold) {                       // check whether battery voltage is below threshold; if so
-      ledcDetach(cCarrierPin);                        // stop carrier signal      
-      esp_deep_sleep_start();                         // and put device to sleep -> unplug and recharge
-    }
+    nextBurst += cBurstInterval;                      // update time for next burst
   }
 
-  // if transmission has been deactivated long enough
-  if (!runState && (curTime - restartTime < cOffTime)) {
-    runState = true;                                  // reactivate transmission
+#ifdef SELF_TEST
+  while (Serial1.available() > 0) {                   // if received data in buffer
+    receivedData = Serial1.read();                    // read a byte (character)
+    Serial.printf("%c\n", receivedData);              // output to serial
   }
+#endif
+#ifdef INCREASE_POWER_USE  
+  if ((curTime - nextScan) < cScanInterval) {         // wait for next scan
+    nextScan += cScanInterval;                        // update time for next scan
+    // alternate between scanning for WiFi networks and deleting list
+    if (!scanState) {                                  
+      WiFi.scanNetworks(true);                        // scan for WiFi networks
+      scanState = true;
+    }
+    else {
+      WiFi.scanDelete();                              // delete scan result to free memory
+      scanState = false;
+    }
+  }
+#endif
 }
